1dce70a319b613483ae7a1836fbc0059dc2a76d9
Alex <115188815+KdimAlex@users.noreply.github.com>
2024-03-03 13:27:01 -0500

    For testing purposes with limit to not exceed twts

---
+++ Test_Run_of_Twitter_Scraper.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import os
+
+def create_sorted_ids_file(data_file):
+    with open(data_file, 'r') as file:
+        ids = file.readlines()
+
+    sorted_ids = sorted(ids, key=lambda x: int(x.strip()), reverse=True)
+
+    dir_path = os.path.dirname(data_file)
+    sorted_file_path = os.path.join(dir_path, 'sorted_ids.txt')
+
+    with open(sorted_file_path, 'w') as file:
+        for id_ in sorted_ids:
+            file.write(f"{id_}")
+
+    return sorted_file_path
+
+def loop_through_ids(data_file_path, bearer_token, limit=None):
+    with open(data_file_path, 'r') as file:
+        ids = file.readlines()
+        print(f"Total IDs: {len(ids)}")
+
+    counter = 0
+    for id_ in ids:
+        if limit is not None and counter >= limit:
+            break  # Stop processing if the limit is reached
+
+        id_ = id_.strip()  # Remove newline characters
+        url = f"https://api.twitter.com/2/tweets/{id_}"
+        headers = {"Authorization": bearer_token}
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            tweet = json.loads(response.text)
+            save_tweet_data(tweet, counter)
+            counter += 1
+        else:
+            print(f"Failed to fetch tweet ID {id_}: HTTP {response.status_code}")
+
+def save_tweet_data(tweet, counter):
+    filename = 'X.data.file' + str(int(counter / 100000))
+    with open(filename, 'a') as fp:
+        json.dump(tweet, fp, indent=4)
+
+def main():
+    bearer_token = "Bearer " + input("Please enter the bearer token for X: ")
+    data_file_path = input("Please enter the data file path you want to parse: ")
+    
+    test_run = input("Do you want to perform a test run? (yes/no): ").lower() == 'yes'
+    limit = int(input("How many tweets for the test run?: ")) if test_run else None
+
+    sorted_data_file = create_sorted_ids_file(data_file_path)
+    loop_through_ids(sorted_data_file, bearer_token, limit)
+
+if __name__ == "__main__":
+    main()
